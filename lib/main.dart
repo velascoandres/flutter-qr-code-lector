c07b0ea51951dbb817b0197e365a82c0890b4200
andres velasco <velasco.andrs@gmail.com>
2020-10-23 23:16:01 -0500

    leer codigos qr

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
- 
+import 'package:qr_code_lector/src/pages/home_page.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      debugShowCheckedModeBanner: false,
+      title: 'QRReader',
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => HomePage(),
+      },
+      theme: ThemeData( 
+        primaryColor: Colors.deepPurple,
       ),
     );
   }
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_lector/src/pages/mapas_page.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+
+import 'direcciones_page.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int paginaActual = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR Scanner'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: _callPage(paginaActual),
+      ),
+      bottomNavigationBar: _crearBottomNavigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.filter_center_focus),
+        onPressed: _scanQR,
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+      default:
+        return MapasPage();
+    }
+  }
+
+  Widget _crearBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: paginaActual,
+      onTap: (index) {
+        paginaActual = index;
+        setState(() {});
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.map,
+          ),
+          label: 'Mapas',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.brightness_5,
+          ),
+          label: 'Dirreciones',
+        ),
+      ],
+    );
+  }
+
+  _scanQR() async {
+    // https://github.com/
+    // geo:40.2323, -73.131231
+    ScanResult resultado;
+    String futureString = '';
+    try {
+      resultado = await BarcodeScanner.scan();
+      futureString = resultado.rawContent;
+    } catch (e) {
+      futureString = e.toString();
+    }
+    print(futureString);
+  }
+}
