830ea13c6869d1cbd754081a3f380cd35cebf226
andres velasco <velasco.andrs@gmail.com>
2020-10-25 12:18:48 -0500

    integrar dbProvider en scanBloc

--- lib/src/bloc/scans_bloc.dart
+++ lib/src/bloc/scans_bloc.dart
@@ -7,7 +7,7 @@ class ScansBloc {
 
   final _scanStreamController = StreamController<List<ScanModel>>.broadcast();
 
-  Stream<List<ScanModel>>get scansStream => _scanStreamController.stream;
+  Stream<List<ScanModel>> get scansStream => _scanStreamController.stream;
 
   factory ScansBloc() {
     return _scansBloc;
@@ -15,10 +15,31 @@ class ScansBloc {
 
   ScansBloc._internal() {
     // Obtener los scans de la base de datos
+    obtenerScans();
   }
 
   dispose() {
     _scanStreamController?.close();
+  }
+
+  void obtenerScans() async {
+    final scans = await DBProvider.db.obtenerScans();
+    _scanStreamController.sink.add(scans);
+  }
+
+  void agregarScan(ScanModel scan) async {
+    await DBProvider.db.nuevoScan(scan);
+    obtenerScans();
+  }
+
+  void borrarScan(int id) async {
+    await DBProvider.db.eliminarRegistro(id);
+    obtenerScans();
+  }
+
+  void borrarScanTodo() async {
+    await DBProvider.db.eliminarTodo();
+    obtenerScans();
   }
 }
 
